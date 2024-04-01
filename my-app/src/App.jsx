222b3ac5ee5fc9f99d805f2fbb5ad97241d3b6de
Suren <meliqbekyan.suren@mail.ru>
2024-04-02 01:09:24 +0400

    popoxutyun

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,9 +1,11 @@
+import { SingleCard } from './components/SingleCard';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <h1>Memory Game - React</h1>
+      <SingleCard/>
     </div>
   );
 }
---
+++ my-app/src/components/Cards.jsx
@@ -0,0 +1,11 @@
+
+
+export const Cards = ({item, id, handleClick }) =>{
+
+    const itemClass = item.stat ? "active" + item.stat : "";
+    return (
+        <div className={"card" + itemClass} onClick={() => handleClick(id)}>
+            <img src={item.img} alt=""/>
+        </div>
+    )
+}
